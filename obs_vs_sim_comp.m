49d922d818a2ab55b332d9d66d39152e87af89a9
Niels Kloosterman <kloosterman@mpib-berlin.mpg.de>
2022-09-29 17:37:54 +0200

    Update obs_vs_sim_comp.m

--- obs_vs_sim_comp.m
+++ obs_vs_sim_comp.m
@@ -1,10 +1,14 @@
 %% quantile prob plots of observed vs simulated data
 
-% simdata = readtable('/Users/kloosterman/Dropbox/PROJECTS/COBRA/hddm/123back_bias_novelvsfam/data/data_simulated.csv');
-% obsdata = readtable('/Users/kloosterman/Dropbox/PROJECTS/COBRA/hddm/123back_bias_novelvsfam/data/data_observed.csv');
+% simdata = readtable('/Users/kloosterman/Dropbox/PROJECTS/COBRA/hddm/123back_bias_novelvsfam/data/data_simulated_fullbiasmodel.csv');
+% obsdata = readtable('/Users/kloosterman/Dropbox/PROJECTS/COBRA/hddm/123back_bias_novelvsfam/data/data_observed_fullbiasmodel.csv');
 
-simdata = readtable('/Users/kloosterman/Dropbox/PROJECTS/COBRA/hddm/123back_bias_novelvsfam/data/data_simulated_basicmodel.csv');
-obsdata = readtable('/Users/kloosterman/Dropbox/PROJECTS/COBRA/hddm/123back_bias_novelvsfam/data/data_observed_basicmodel.csv');
+% simdata = readtable('/Users/kloosterman/Dropbox/PROJECTS/COBRA/hddm/123back_bias_novelvsfam/data/data_simulated_basicmodel.csv');
+% obsdata = readtable('/Users/kloosterman/Dropbox/PROJECTS/COBRA/hddm/123back_bias_novelvsfam/data/data_observed_basicmodel.csv');
+
+PREIN = '/Users/kloosterman/Dropbox/PROJECTS/COBRA/hddm/123back_bias_novelvsfam/data/';
+modelnames = {'avt model' 'avt + dc + z model'};
+model_filenames = {'basicmodel.csv', 'fullbiasmodel.csv' };
 
 %% make quantiles observed data
 % quantiles = [0 20 40 60 80 100];
@@ -13,52 +17,61 @@ quantiles = [10 20 40 60 80 90];
 % quantiles = [0.5 10 30 50 70 90 99.5];
 nbins = length(quantiles)-1;
 
-SUBJ = unique(obsdata.subj_idx);
-
-% for observed data
-% includes stim non-target or target: want plots for stim present and
-% absent
 bindat = [];
-for isub = 1:length(SUBJ)
-  for istim = 0:1 % non-target or target
-    for icond = 2:3 % only 2 and 3 back
-      line_inds = obsdata.subj_idx == SUBJ(isub) & obsdata.stim == icond & obsdata.stimulus == istim;
-      dat = [abs(obsdata.rt(line_inds)) obsdata.accuracy(line_inds) ]; % we want choice (0 is no), not accuracy
-      prctile_edges = prctile(dat(:,1), quantiles);       % get RT quantiles
-      [~,~,bin] = histcounts(dat(:,1), prctile_edges); % get bin that each trial belongs to
-      dat = [dat bin]; % append      
-      for ibin = 1:nbins % get proportion responses per quantile
-        for icorr = 0:1 % incorrect and correct
-          bindat(isub, istim+1, ibin, icond-1, icorr+1, 1 ) = mean(dat(dat(:,3)==ibin & dat(:,2) == icorr, 1)); %mean RT per bin and correct/incorrect
-          bindat(isub, istim+1, ibin, icond-1, icorr+1, 2 ) = length(dat(dat(:,3)==ibin & dat(:,2) == icorr, 2)) ./ length(find(dat(:,3)==ibin)); %correctness
-        end
-      end
-    end
-  end
-end
-
-% for simulated data sets (50)
-% includes stim non-target or target: want plots for stim present and
-% absent
-datasets_sim = unique(simdata.sample);
-
 bindat_sim = [];
-for idata = 1:10%length(datasets_sim)
-  disp(idata)
-  curdat = simdata(simdata.sample==datasets_sim(idata),:);
+for imodel = 1:length(modelnames)
+  
+  simdata = readtable(fullfile(PREIN, ['data_simulated_' model_filenames{imodel}] ));
+  simdata = simdata(abs(simdata.rt) < 1.5,:);
+  
+  obsdata = readtable(fullfile(PREIN, ['data_observed_' model_filenames{imodel}] ));
+  
+  SUBJ = unique(obsdata.subj_idx);
+  
+  % for observed data
+  % includes stim non-target or target: want plots for stim present and
+  % absent
   for isub = 1:length(SUBJ)
     for istim = 0:1 % non-target or target
       for icond = 2:3 % only 2 and 3 back
-        line_inds = strcmp( sprintf('wfpt(%d.%d).%d', icond, istim, SUBJ(isub)), simdata.node ) & simdata.sample == datasets_sim(idata); 
-%         line_inds = simdata.subj_idx == SUBJ(isub) & simdata.stim == icond & simdata.stimulus == istim;
-        dat = [abs(simdata.rt(line_inds )) simdata.response(line_inds) ]; 
+        line_inds = obsdata.subj_idx == SUBJ(isub) & obsdata.stim == icond & obsdata.stimulus == istim;
+        dat = [abs(obsdata.rt(line_inds)) obsdata.accuracy(line_inds) ]; % we want choice (0 is no), not accuracy
         prctile_edges = prctile(dat(:,1), quantiles);       % get RT quantiles
         [~,~,bin] = histcounts(dat(:,1), prctile_edges); % get bin that each trial belongs to
         dat = [dat bin]; % append
         for ibin = 1:nbins % get proportion responses per quantile
           for icorr = 0:1 % incorrect and correct
-            bindat_sim(isub, idata, istim+1, ibin, icond-1, icorr+1, 1 ) = median(dat(dat(:,3)==ibin & dat(:,2) == icorr, 1)); %mean RT per bin and correct/incorrect
-            bindat_sim(isub, idata, istim+1, ibin, icond-1, icorr+1, 2 ) = length(dat(dat(:,3)==ibin & dat(:,2) == icorr, 2)) ./ length(find(dat(:,3)==ibin)); %correctness
+            bindat(imodel,isub, istim+1, ibin, icond-1, icorr+1, 1 ) = median(dat(dat(:,3)==ibin & dat(:,2) == icorr, 1)); %mean RT per bin and correct/incorrect
+            bindat(imodel,isub, istim+1, ibin, icond-1, icorr+1, 2 ) = length(dat(dat(:,3)==ibin & dat(:,2) == icorr, 2)) ./ length(find(dat(:,3)==ibin)); %correctness
+          end
+        end
+      end
+    end
+  end
+  
+  % for simulated data sets (50)
+  % includes stim non-target or target: want plots for stim present and
+  % absent
+  datasets_sim = unique(simdata.sample);
+  
+  for idata = 1:10%length(datasets_sim)
+    disp(idata)
+    curdat = simdata(simdata.sample==datasets_sim(idata),:);
+    for isub = 1:length(SUBJ)
+      for istim = 0:1 % non-target or target
+        for icond = 2:3 % only 2 and 3 back
+          line_inds = strcmp( sprintf('wfpt(%d.%d).%d', icond, istim, SUBJ(isub)), simdata.node ) & simdata.sample == datasets_sim(idata);
+          %         line_inds = simdata.subj_idx == SUBJ(isub) & simdata.stim == icond & simdata.stimulus == istim;
+          dat = [abs(simdata.rt(line_inds )) simdata.response(line_inds) ];
+          
+          prctile_edges = prctile(dat(:,1), quantiles);       % get RT quantiles
+          [~,~,bin] = histcounts(dat(:,1), prctile_edges); % get bin that each trial belongs to
+          dat = [dat bin]; % append
+          for ibin = 1:nbins % get proportion responses per quantile
+            for icorr = 0:1 % incorrect and correct
+              bindat_sim(imodel,isub, idata, istim+1, ibin, icond-1, icorr+1, 1 ) = median(dat(dat(:,3)==ibin & dat(:,2) == icorr, 1)); %mean RT per bin and correct/incorrect
+              bindat_sim(imodel,isub, idata, istim+1, ibin, icond-1, icorr+1, 2 ) = length(dat(dat(:,3)==ibin & dat(:,2) == icorr, 2)) ./ length(find(dat(:,3)==ibin)); %correctness
+            end
           end
         end
       end
@@ -66,52 +79,56 @@ for idata = 1:10%length(datasets_sim)
   end
 end
 
-  
+
 %% plot quantile prob plot
-bindat_avg = squeeze(nanmean(bindat)); % stim bins cond incorrect/correct RT/accuracy
-bindat_sim_avg = squeeze(nanmean(bindat_sim)); % stim bins cond incorrect/correct RT/accuracy
+bindat_avg = squeeze(nanmean(bindat,2)); % stim bins cond incorrect/correct RT/accuracy
+bindat_sim_avg = squeeze(nanmean(bindat_sim,2)); % stim bins cond incorrect/correct RT/accuracy
 titnames = {'Target absent' 'Target present'};
 SAV=1;
-YLIM = [0.5 1.5];
-f = figure; f.Position = [        1000        1115         625         150];
-quantile_leg = {};
-for istim = 1:2 % absent, present
-  subplot(1,2,istim); hold on
-  title(titnames{istim})
-  xlabel('Proportion of responses');
-  ylabel('Reaction time (s)')
-  ylim(YLIM)
-  % plot the observed data
-  for ibin = 1:nbins
-    
-    dat_x = squeeze(bindat_avg(istim, ibin,:,:,2 )); % RT for both conditions, correct and incorrect
-    dat_y = squeeze(bindat_avg(istim, ibin,:,:,1 )); % RT for both conditions, correct and incorrect
-    dat_x(:,2) = flipud(dat_x(:,2)); % to get the line straight through the points
-    dat_y(:,2) = flipud(dat_y(:,2));
-
-    plot(dat_x(:), dat_y(:), '-x')
-%     quantile_leg{ibin} = sprintf('q %1.2f', quantiles(ibin+1)/100-0.1);
-    quantile_leg{ibin} = sprintf('q %d-%d', quantiles(ibin), quantiles(ibin+1) );
-  end
-  for idata = 1:size(bindat_sim_avg,1)
-    % plot the sim data
+YLIM = [0.5 1.3];
+f = figure; f.Position = [        1000        1115         625         150*length(modelnames)];
+iplot=0;
+for imodel = 1:length(modelnames)
+  quantile_leg = {};
+  for istim = 1:2 % absent, present
+    iplot=iplot+1;
+    subplot(length(modelnames),2,iplot); hold on
+    title(sprintf('%s: %s', modelnames{imodel}, titnames{istim}))
+    xlabel('Proportion of responses');
+    ylabel('Reaction time (s)')
+    ylim(YLIM)
+    % plot the observed data
     for ibin = 1:nbins
       
-      dat_x = squeeze(bindat_sim_avg(idata,istim, ibin,:,:,2 )); % RT for both conditions, correct and incorrect
-      dat_y = squeeze(bindat_sim_avg(idata,istim, ibin,:,:,1 )); % RT for both conditions, correct and incorrect
+      dat_x = squeeze(bindat_avg(imodel, istim, ibin,:,:,2 )); % RT for both conditions, correct and incorrect
+      dat_y = squeeze(bindat_avg(imodel, istim, ibin,:,:,1 )); % RT for both conditions, correct and incorrect
       dat_x(:,2) = flipud(dat_x(:,2)); % to get the line straight through the points
       dat_y(:,2) = flipud(dat_y(:,2));
       
-      plot(dat_x(:), dat_y(:), '.k')
-%       quantile_leg{ibin} = sprintf('q %1.2f', quantiles(ibin+1)/100-0.1);
+      plot(dat_x(:), dat_y(:), '-x')
+      %     quantile_leg{ibin} = sprintf('q %1.2f', quantiles(ibin+1)/100-0.1);
+      quantile_leg{ibin} = sprintf('q %d-%d', quantiles(ibin), quantiles(ibin+1) );
     end
+    for idata = 1:size(bindat_sim_avg,2)
+      % plot the sim data
+      for ibin = 1:nbins
+        
+        dat_x = squeeze(bindat_sim_avg(imodel,idata,istim, ibin,:,:,2 )); % RT for both conditions, correct and incorrect
+        dat_y = squeeze(bindat_sim_avg(imodel,idata,istim, ibin,:,:,1 )); % RT for both conditions, correct and incorrect
+        dat_x(:,2) = flipud(dat_x(:,2)); % to get the line straight through the points
+        dat_y(:,2) = flipud(dat_y(:,2));
+        
+        plot(dat_x(:), dat_y(:), '.k')
+        %       quantile_leg{ibin} = sprintf('q %1.2f', quantiles(ibin+1)/100-0.1);
+      end
+    end
+    %   if istim==2
+    legend(quantile_leg, 'Location', 'EastOutside'); legend boxon
+    %   end
+    ax=gca;
+    ax.FontSize = 8;
+    %   line()
   end
-  %   if istim==2
-  legend(quantile_leg, 'Location', 'EastOutside'); legend boxon
-  %   end
-  ax=gca;
-  ax.FontSize = 8;
-%   line()
 end
 
 % legend off
